006407e523e3a6db3160b6e566ee504e169c3c96
lucy0511d <luxiaagzl1105@gmail.com>
2024-12-04 20:52:29 +0100

    uu

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    private Outline outline;
+    [SerializeField] private Texture2D cursorInteraccion;
+    [SerializeField] private Texture2D cursorPorDefecto;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        outline = GetComponent<Outline>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseEnter()
     {
-        
+        Cursor.SetCursor(cursorInteraccion, Vector2.zero, CursorMode.Auto); //Vector2.zero = (0,0)
+        outline.enabled = true;
+    }
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorPorDefecto, Vector2.zero, CursorMode.Auto);
+        outline.enabled = false;
     }
 }
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,12 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float distanciaInteraccion;
+
     private NavMeshAgent agent;
     private Camera cam;
+    //Guardo la informacion del NPC actual con el que voy a hablar
+    private NPC npcActual;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +21,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        Movimiento();
+        //Si existe un npc al cual clicke...
+        if(npcActual)
+        {
+            //Comprobar si he llegado al NPC
+            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                Debug.Log("Hola!");
+                npcActual = null;
+            }
+        }
+        
+    }
+
+    private void Movimiento()
+    {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition); // te encuentra la posicion del boton en pantalla
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             if (Input.GetMouseButtonDown(0))
             {
+                //Mirar si el punto donde he impactado tiene el script "NPC"
+                if (hit.transform.TryGetComponent(out NPC npc))
+                {
+                    //Y en ese caso, ese npc es el actual
+                    npcActual = npc;
+                    // Ahora mi distancia de parada es la de interaccion (pararme a X metros del NPC)
+                    agent.stoppingDistance = distanciaInteraccion;
+                }
+
                 agent.SetDestination(hit.point); //hit.point el punto del impacto
             }
         }
