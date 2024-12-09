9be3b0ab86a15c6761e28c53286e82a0c27a6878
lucy0511d <luxiaagzl1105@gmail.com>
2024-12-09 17:27:42 +0100

    SSS

--- Assets/Scripts/SistemaDeDialogos.cs
+++ Assets/Scripts/SistemaDeDialogos.cs
@@ -17,6 +17,8 @@ public class SistemaDeDialogos : MonoBehaviour
     private bool escribiendo; //Determina si el sistema está escribiendo o no.
     private int indiceFraseActual; //Marca la frase por la que voy.
 
+    private DialogoSO dialogoActual; // Para almacenar con qué dialogo estamos trabajando
+
     private void Awake()
     {
         // si el trono esta vacio...
@@ -35,12 +37,22 @@ public class SistemaDeDialogos : MonoBehaviour
     }
     public void IniciarDialogo(DialogoSO dialogo)
     {
+        //El dialgo actual con el que trabajamos es el que me dan por parámetro de entrada.
+        dialogoActual = dialogo;
         marcos.SetActive(true);
+        StartCoroutine(EscribirFrase());
     }
     //Que el texto aparezca letra por letra
-    private void EscribirFrase()
+    private IEnumerator EscribirFrase()
     {
+        //"Hola aventurero te esperan grandes aventuras"
+        char[] fraseEnLetras= dialogoActual.frases[indiceFraseActual].ToCharArray();
+        foreach (char letra in fraseEnLetras)
+        {
+            textoDialogo.text += letra;
+            yield return new WaitForSeconds(0.02f);
 
+        }
     }
     private void SiguienteFrase()
     {
