3ccb27076e60dbc5371bd305908b4228f35f2e2c
DESKTOP-GV16N45\Alumno <luxiaagzl1105@gmail.com>
2025-01-27 18:10:10 +0100

    rwe

--- Assets/Scripts/SistemaMisiones.cs
+++ Assets/Scripts/SistemaMisiones.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SistemaMisiones : MonoBehaviour
 {
     [SerializeField] private EventManagerSO eventManager;
-    [SerializeField] private GameObject toggleMision;
+    [SerializeField] private ToggleMision[] toggleMision;
     private void OnEnable()
     {
         eventManager.OnNuevaMision += ActivarToggleMision;//te suscribes
@@ -14,6 +15,11 @@ public class SistemaMisiones : MonoBehaviour
 
     private void ActivarToggleMision(MisionSO mision)
     {
-        toggleMision.SetActive(true);
+        toggleMision[mision.indiceMision].TextoMision.text = mision.ordenInicial;
+        if(mision.repetir)
+        {
+            toggleMision[mision.indiceMision].TextoMision.text += "(" + mision.estadoActual + "/" + mision.repeticionesTotales + ")";
+        }
+        toggleMision[mision.indiceMision].gameObject.SetActive(true);
     }
 }
